43fda12416f30ea496dae47c47541aff8ad563ba
Achour Bensouna <achourbensouna100@gmail.com>
2024-12-04 17:14:23 +0100

    it s done el hamdoulilah

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 # from flask_migrate import Migrate
-from flask_caching import Cache
+from flask_wtf.csrf import CSRFProtect
+from flask_compress import Compress
 import os
+
 
 
 
@@ -18,11 +20,16 @@ if DATABASE_URL.startswith("postgres://"):
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'w5vz1o9vu*rr-mancubpwbh&k6uww&%f3!yr#n^od-zokit#9v'
+app.config['COMPRESS_MIMETYPES'] = ['text/html', 'application/json', 'text/css', 'application/javascript']
+app.config['COMPRESS_LEVEL'] = 6  # Compression level, from 1 (fastest) to 9 (best compression)
+app.config['COMPRESS_MIN_SIZE'] = 500  # Only compress responses larger than this size in bytes
+csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 app.config['CACHE_TYPE'] = 'RedisCache'
 app.config['CACHE_REDIS_URL'] = os.getenv("CACHE_REDIS_URL")
+Compress(app)
 
-cache = Cache(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'  # Redirect unauthorized users to the login page
@@ -51,10 +58,47 @@ class FormData(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(20), default='pending')  # New status field
 
+
+
+# class OrderForm(FlaskForm):
+#     # Color (Radio Buttons)
+#     color = RadioField('اختر اللون', choices=[('bordeau', 'بوردو'), ('beige', 'بيج'), ('black', 'أسود')],
+#                        validators=[DataRequired()])
+
+#     # Shoe Size (Radio Buttons)
+#     size = RadioField('اختر المقاس', choices=[('36', '36'), ('37', '37'), ('38', '38'), ('39', '39')],
+#                       default='36', validators=[DataRequired()])
+
+#     # Name
+#     name = StringField('الاسم', validators=[DataRequired(), Length(min=2, max=50)])
+
+#     # Phone Number
+#     phone = StringField('رقم الهاتف', validators=[DataRequired(), Length(min=10, max=15)])
+
+#     # State (Dropdown)
+#     state = SelectField('اختر ولايتك', choices=[
+#         ('', '-- اختر الولاية --'),
+#         ('Adrar', 'أدرار'),
+#         ('Chlef', 'الشلف'),
+#         ('Laghouat', 'الأغواط'),
+#         ('Oum El Bouaghi', 'أم البواقي'),
+#         ('Batna', 'باتنة'),
+#         # Add other states here...
+#     ], validators=[DataRequired()])
+
+#     # Address
+#     address = StringField('العنوان', validators=[DataRequired(), Length(min=5, max=100)])
+
+#     # Quantity (with default value 1)
+#     quantity = IntegerField('الكمية', validators=[DataRequired(), NumberRange(min=1)], default=1)
+
+#     # Submit Button
+#     submit = SubmitField('تأكيد الطلبية')
 # Load user callback for Flask-Login
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user = db.session.get(User, int(user_id))
+    return user
 
 # Route for login
 @app.route('/login', methods=['GET', 'POST'])
@@ -119,11 +163,15 @@ def update_status(order_id):
 
     return redirect(url_for('sales'))
 
+
 # Route for the form
 @app.route('/product', methods=['GET', 'POST'])
-@cache.cached(timeout=300)
+# @cache.cached(timeout=300)
 def product():
+    print('iam in the method')
     if request.method == 'POST':
+        print('iam in the if')
+        # CSRF protection is automatically handled
         # Get form data
         name = request.form['name']
         phone = request.form['phone']
@@ -132,10 +180,10 @@ def product():
         color = request.form['color']
         size = request.form['size']
         quantity = request.form['quantity']
-        
+        print(name, phone, address, state, color, size, quantity)
 
         # Save to database
-        form_data = FormData(
+        form_data =FormData(
             name=name,
             phone=phone,
             address=address,
@@ -143,15 +191,15 @@ def product():
             color=color,
             size=size,
             quantity=quantity,
-            
         )
         db.session.add(form_data)
         db.session.commit()
 
         flash(f'.شكرا على اختيارنا, سنتواصل معك قريبا ', 'success')
         return redirect(url_for('product'))
-
     return render_template('product-detail.html')
+
+  
 
 
 @app.route("/")
@@ -159,11 +207,7 @@ def home():
     return render_template('index.html')
 
 # Initialize thedatabase
-with app.app_context():
-    db.create_all()
-    new_user = User(username="achour", password="dammiette123")  # Replace with hashed passwords in production
-    db.session.add(new_user)
-    db.session.commit()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
